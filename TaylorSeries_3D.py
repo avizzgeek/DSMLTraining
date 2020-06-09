aa02debc06a287949011f28f97b4f051a9a08261
Avinandan Behera <reach2avinandan@gmail.com>
2020-06-09 18:02:04 +0530

    Update TaylorSeries_3D.py

--- TaylorSeries_3D.py
+++ TaylorSeries_3D.py
@@ -2,26 +2,48 @@ import sympy as sym
 from sympy import Subs, Function
 import numpy as np
 import math
+from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 
+
 x = sym.Symbol('x')
+y = sym.Symbol('y')
 
 Yarray = [ ]
 Xarray = [ ]
-
 TYarray = [ ]
-TXarray = [ ]
-def plot_graph(Yarray,Xarray,linecolor):
-    plt.plot(Xarray,Yarray,color=linecolor)
-    plt.ylabel('Nth order Derivative')
-    plt.xlabel('X Value')
-    plt.show()
 
 
-def find_value_of_equation(expr, value):
-    substitutedValue = expr.subs(x, value)
+
+def find_value_of_equation(expr, xval,yval):
+    substitutedValue = expr.subs(x, xval)
+    substitutedValue = substitutedValue.subs(y,yval)
+
     return substitutedValue
 
+
+def findDirDerivative(expression,xcod,ycod,zcod):
+    #expression
+    x, y, z = sym.symbols('x y z')
+    #lhs = expression.split("=")[0]
+
+    grad1 = sym.diff(expression, x)
+    grad2 = sym.diff(expression, y)
+    grad3 = sym.diff(expression, z)
+    gradList = [ ]
+    gradList.append(grad1.subs(x, xcod))
+    gradList.append(grad2.subs(y, ycod))
+    gradList.append(grad3.subs(z, zcod))
+
+    unitVector = [ ]
+    sqr = np.sqrt(xcod**2+ycod**2+zcod**2)
+    unitVector.append(xcod/sqr)
+    unitVector.append(ycod/sqr)
+    unitVector.append(zcod/sqr)
+
+    directionaDerivative = gradList[0]*unitVector[0] + gradList[1]*unitVector[1] + gradList[2]*unitVector[2]
+
+    return directionaDerivative
 
 def find_taylor_Expansion(expr,xvalue,avalue):
     f0 = expr.subs(x, xvalue)
@@ -38,37 +60,33 @@ def find_taylor_Expansion(expr,xvalue,avalue):
 
 def driverCode(expr):
     #substitue different values of x
-    '''for i in np.arange(-1,6,0.5):
-        Yarray.append(find_value_of_equation(expr,i))
-        Xarray.append(i)
-        if (i >= -1 and i < 1):
-            TYarray.append(find_taylor_Expansion(expr, i, 0))
-        elif (i >= 1 and i < 2):
-            TYarray.append(find_taylor_Expansion(expr, i, 1))
-        elif (i >= 2 and i < 3):
-            TYarray.append(find_taylor_Expansion(expr, i, 2))
-        elif (i >= 3 and i < 4):
-            TYarray.append(find_taylor_Expansion(expr, i, 3))
-        elif (i >= 4 and i < 5):
-            TYarray.append(find_taylor_Expansion(expr, i, 4))
-        elif (i >= 5 and i < 6):
-            TYarray.append(find_taylor_Expansion(expr, i, 5))'''
+    xvallist = []
+    yvallist = []
+    zvallist = []
     for aval in np.arange(0,5,0.5):
         min = aval - 0.5
         max = aval + 0.5
         i = 0
         for xval in np.arange(min,max,0.1):
-            Yarray.append(find_value_of_equation(expr, xval))
-            Xarray.append(xval)
-            TYarray.append(find_taylor_Expansion(expr, xval, aval))
+            for yval in np.arange(min,max,0.1):
+                zval = find_value_of_equation(expr, xval,yval)
+                xvallist.append(int(xval))
+                yvallist.append(int(yval))
+                zvallist.append(int(zval))
 
-    print("{0},{1}".format(len(TYarray),len(Xarray)))
-    plt.plot(Xarray,Yarray,'red')
-    plt.plot(Xarray,TYarray,'blue')
-    plt.ylabel('Nth order Derivative')
-    plt.xlabel('X Value')
+
+
+    print("{0},{1},{2}".format(xvallist,yvallist,zvallist))
+    # Data for three-dimensional scattered points
+
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x, y, z = axes3d.get_test_data(0.05)
+    ax.plot_wireframe(x, y, z, rstride=5, cstride=5)
     plt.show()
 
+
 if __name__ == "__main__":
-    expr = 3 * x ** 3 + x ** 2 - 8 * x + 6
+    expr = x**5+y**5
     driverCode(expr)
